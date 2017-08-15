0cb736aaafdfba03ffcd9b042531e9d8372e28e5
wudi <wudi04@b.360.cn>
2017-08-15 10:48:22 +0800

    添加背景音乐

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,39 +5,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <title>Mr.Wu & Miss.Bai</title>
     <link rel="stylesheet" href="/css/swiper.css">
+    <link rel="stylesheet" href="http://wudihunsha.oss-cn-shanghai.aliyuncs.com/music.css">
     <script src="http://wudihunsha.oss-cn-shanghai.aliyuncs.com/public/js/swiper.js"></script>
-  <style>
-   .swiper-container {
-        width: 100%;
-        height: 100%;
-    }
-    .swiper-slide {
-        font-size: 18px;
-        background: #fff;
+    <style>
+        .swiper-container {
+            width: 100%;
+            height: 100%;
+        }
+        .swiper-slide {
+            font-size: 18px;
+            background: #fff;
 
-        /* Center slide text vertically */
-        display: -webkit-box;
-        display: -ms-flexbox;
-        display: -webkit-flex;
-        display: flex;
-        -webkit-box-pack: center;
-        -ms-flex-pack: center;
-        -webkit-justify-content: center;
-        justify-content: center;
-        -webkit-box-align: center;
-        -ms-flex-align: center;
-        -webkit-align-items: center;
-        align-items: center;
-    }
-    iframe{
-        height:100%;
-        width:100%;
-    }
-  </style>
-  </head>
+            /* Center slide text vertically */
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: -webkit-flex;
+            display: flex;
+            -webkit-box-pack: center;
+            -ms-flex-pack: center;
+            -webkit-justify-content: center;
+            justify-content: center;
+            -webkit-box-align: center;
+            -ms-flex-align: center;
+            -webkit-align-items: center;
+            align-items: center;
+        }
+        iframe{
+            height:100%;
+            width:100%;
+        }
+    </style>
+    </head>
   
-  <body>
-  <div class="swiper-container">
+    <body>
+    <div id="music" class="music">		
+    <audio id="music-audio" class="audio" src="http://wudihunsha.oss-cn-shanghai.aliyuncs.com/jianpan.mp3" loop="" autoplay="autoplay" preload="auto">		</audio>		
+    <div class="control"><div class="control-after"></div></div>	
+    </div>
+    <div class="swiper-container">
         <div class="swiper-wrapper">
             <div class="swiper-slide">
                 <div id="wedding"></div>
@@ -49,17 +54,25 @@
             </div>
         </div>
    </div>
-     <script>
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        centeredSlides: true,
-        slidesPerView: 'auto',
-        effect: 'coverflow',
-        lazyLoading : true,
-        lazyLoadingInPrevNext : true,
-        lazyLoadingInPrevNextAmount : 2,
-    });
+    <script src="http://wudihunsha.oss-cn-shanghai.aliyuncs.com/public/js/jquery.js"></script>
+    <script>
+        $(function(){
+            $('#music').click(function(){
+                $(this).toggleClass('stopped')
+            })
+        });
+    </script>
+    <script>
+        var swiper = new Swiper('.swiper-container', {
+            pagination: '.swiper-pagination',
+            paginationClickable: true,
+            centeredSlides: true,
+            slidesPerView: 'auto',
+            effect: 'coverflow',
+            lazyLoading : true,
+            lazyLoadingInPrevNext : true,
+            lazyLoadingInPrevNextAmount : 2,
+        });
     </script>
 
   </body>
